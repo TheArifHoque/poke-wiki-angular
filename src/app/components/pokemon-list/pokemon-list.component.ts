75bff3576bc1efd7d13864d5fed4ba5283aeb4a3
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-02-18 23:55:24 +0600

    pokemon-list service updated

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from '../../service/poke-api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SharedDataService } from '../../service/shared-data.service';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,20 +9,50 @@ import { PokeApiService } from '../../service/poke-api.service';
   styleUrl: './pokemon-list.component.scss'
 })
 export class PokemonListComponent implements OnInit {
+  currentPageNo: number;
   pokemonDetails: any[];
 
-  constructor(private pokeApiService: PokeApiService) {
+  constructor(private pokeApiService: PokeApiService, 
+    private router: Router, 
+    private route: ActivatedRoute, 
+    private sharedDataService: SharedDataService) {
+    this.currentPageNo = 0;
     this.pokemonDetails = [];
   }
 
   ngOnInit(): void {
-    this.loadAllPokemonDetails();
+    this.route.queryParamMap.subscribe((params: any) => {
+      this.currentPageNo = params.has('page') ? params.get('page') : 0;
+      this.loadAllPokemonDetails();
+    });
+    // this.loadAllPokemonDetails();
   }
 
   loadAllPokemonDetails(): void {
-    this.pokeApiService.getPokemonDetails(0).subscribe({
+    this.pokeApiService.getPokemonDetails(this.currentPageNo).subscribe({
       next: (result) => this.pokemonDetails = result,
       error: (error) => console.error(error),
     });
+  }
+
+  loadPreviousPage(): void {
+    this.router.navigate(['/pokemon-list'], {
+      queryParams: {
+        page: --this.currentPageNo,
+      }
+    });
+  }
+
+  loadNextPage(): void {
+    this.router.navigate(['/pokemon-list'], {
+      queryParams: {
+        page: ++this.currentPageNo,
+      }
+    });
+  }
+
+  viewDetails(pokemon: any): void {
+    this.sharedDataService.sendData(pokemon);
+    this.router.navigate(['/pokemon-details']);
   }
 }
