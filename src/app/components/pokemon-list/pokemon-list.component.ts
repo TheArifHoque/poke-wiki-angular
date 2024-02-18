b5a33aa7a6c4c1d3677250a1f96df92e0b3b0a81
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-02-18 12:47:58 +0600

    loaded all pokemon

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from '../../service/poke-api.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss'
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
+  pokemonDetails: any[];
 
+  constructor(private pokeApiService: PokeApiService) {
+    this.pokemonDetails = [];
+  }
+
+  ngOnInit(): void {
+    this.loadAllPokemonDetails();
+  }
+
+  loadAllPokemonDetails(): void {
+    this.pokeApiService.getPokemonDetails(0).subscribe({
+      next: (result) => this.pokemonDetails = result,
+      error: (error) => console.error(error),
+    });
+  }
 }
