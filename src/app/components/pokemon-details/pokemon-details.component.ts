3a341d3a96a8964485c06b2dd84a269a250bffe0
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-02-19 00:55:25 +0600

    updated pokemon-details

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { SharedDataService } from '../../service/shared-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.scss'
 })
-export class PokemonDetailsComponent {
+export class PokemonDetailsComponent implements OnDestroy {
+  pokemon: any;
+  subscription: Subscription;
 
+  constructor(private sharedDataService: SharedDataService) {
+    this.subscription = sharedDataService.data$.subscribe((data) => (this.pokemon = data));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
